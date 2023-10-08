3860d5a5f54acd6f8d5dd169c120b4d0047788a3
Saman <samangithoseinpour@gmail.com>
2023-10-08 09:59:21 +0330

    implement navbar ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { NavLinks } from '@/constants';
+import { AuthProviders } from '.';
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  const session = {};
+
+  return (
+    <nav className="flexBetween navbar">
+      <div className="flex-1 flexStart gap-10">
+        <Link href="/">
+          <Image
+            src="/favicon.ico"
+            alt="Freelancers Marketplace"
+            width={115}
+            height={43}
+          ></Image>
+        </Link>
+        <ul className="xl:flex hidden gap-7 text-small">
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            UserPhoto
+            <Link href="/create-post">+Share Post</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
