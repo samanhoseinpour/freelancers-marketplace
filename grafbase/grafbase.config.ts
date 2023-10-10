c67e0164b14a44693ee21362d85aecf6c1d1510f
Saman <samangithoseinpour@gmail.com>
2023-10-10 11:37:05 +0330

    connect jwt to grafbase and create rules for schema

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,28 +1,45 @@
 import { g, auth, config } from '@grafbase/sdk';
 
-const User = g.model('User', {
-  name: g.string().length({ min: 2, max: 20 }),
-  email: g.email().unique(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g
-    .relation(() => Project)
-    .list()
-    .optional(),
-});
+// @ts-ignore
+const User = g
+  .model('User', {
+    name: g.string().length({ min: 2, max: 20 }),
+    email: g.email().unique(),
+    avatarUrl: g.url(),
+    description: g.string().optional(),
+    githubUrl: g.url().optional(),
+    linkedInUrl: g.url().optional(),
+    projects: g
+      .relation(() => Project)
+      .list()
+      .optional(),
+  })
+  .auth((rules) => rules.public().read());
 
-const Project = g.model('Project', {
-  title: g.string().length({ min: 3 }),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.string(),
-  catrgory: g.string().search(),
-  createdBy: g.relation(() => User),
+// @ts-ignore
+const Project = g
+  .model('Project', {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.string(),
+    catrgory: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read(), rules.private().create().delete().update();
+  });
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret: g.env('NEXTAUTH_SECRET'),
 });
 
 export default config({
   schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rule) => rule.private(),
+  },
 });
